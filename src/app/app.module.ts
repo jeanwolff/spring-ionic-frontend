3e2807d507713d79556eed5edf986cd3be331b07
Jean Wolff <wolff.jean@live.com>
2018-08-06 19:32:39 -0300

    Listando Categorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 
-import { MyApp } from './app.component';
+import { MyApp } from "./app.component";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-
-
-
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { CategoriaService } from "../services/domain/categoria.service";
 
 @NgModule({
   // Colocando lista de components ou paginas que fazem parte desse modulo
-  declarations: [
-    MyApp
-  ],
+  declarations: [MyApp],
   // Lista de modulos que são importados por esse modulo
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
+  imports: [BrowserModule, HttpClientModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp
-  ],
+  entryComponents: [MyApp],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    CategoriaService
   ]
 })
 export class AppModule {}
