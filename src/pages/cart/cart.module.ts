7d3f82b51c92800bc6d418a90eef300092449ea6
Jean Wolff <wolff.jean@gmail.com>
2018-09-24 20:35:14 -0300

    criando pagina de carrinho de compras

---
+++ src/pages/cart/cart.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+import { CartPage } from "./cart";
+
+@NgModule({
+  declarations: [CartPage],
+  imports: [IonicPageModule.forChild(CartPage)]
+})
+export class CartPageModule {}
--- src/pages/produto-detail/produto-detail.ts
+++ src/pages/produto-detail/produto-detail.ts
@@ -1,6 +1,9 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { ProdutoDTO } from "../../models/produto.dto";
+import { ProdutoService } from "../../services/domain/produto.service";
+import { API_CONFIG } from "../../config/api.config";
+import { CartService } from "../../services/domain/cart.service";
 @IonicPage()
 @Component({
   selector: "page-produto-detail",
@@ -8,13 +11,37 @@ import { ProdutoDTO } from "../../models/produto.dto";
 })
 export class ProdutoDetailPage {
   item: ProdutoDTO;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public produtoService: ProdutoService,
+    public cartService: CartService
+  ) {}
 
   ionViewDidLoad() {
-    this.item = {
-      id: "1",
-      nome: "mouse",
-      preco: 90.0
-    };
+    let produto_id = this.navParams.get("produto_id");
+    this.produtoService.findById(produto_id).subscribe(
+      response => {
+        this.item = response;
+        this.getImageUrlIfExists();
+      },
+      error => {}
+    );
+  }
+
+  getImageUrlIfExists() {
+    this.produtoService.getImageFromBucket(this.item.id).subscribe(
+      response => {
+        this.item.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${
+          this.item.id
+        }.jpg`;
+      },
+      error => {}
+    );
+  }
+
+  addToCart(produto: ProdutoDTO) {
+    this.cartService.addProduto(produto);
+    this.navCtrl.setRoot("CartPage");
   }
 }
