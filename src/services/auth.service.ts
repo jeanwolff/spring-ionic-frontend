f5c87cd58497fce59a811a3f8c1a5abcbe7fbdc4
Jean Wolff <wolff.jean@gmail.com>
2018-09-24 21:13:16 -0300

    Terminando as funcionalidades do Carrinho

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,12 +5,17 @@ import { API_CONFIG } from "../config/api.config";
 import { LocalUser } from "../models/localUser";
 import { StorageService } from "./storage.services";
 import { JwtHelper } from "angular2-jwt";
+import { CartService } from "./domain/cart.service";
 
 @Injectable()
 export class AuthService {
   jwtHelper: JwtHelper = new JwtHelper();
 
-  constructor(public http: HttpClient, public storage: StorageService) {}
+  constructor(
+    public http: HttpClient,
+    public storage: StorageService,
+    public cartService: CartService
+  ) {}
 
   authenticate(creds: CredenciaisDTO) {
     return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {
@@ -26,6 +31,7 @@ export class AuthService {
       email: this.jwtHelper.decodeToken(tok).sub
     };
     this.storage.setLocalUser(user);
+    this.cartService.createOrClearCart();
   }
 
   logout() {
--- src/services/domain/cart.service.ts
+++ src/services/domain/cart.service.ts
@@ -2,6 +2,7 @@ import { StorageService } from "../storage.services";
 import { Injectable } from "@angular/core";
 import { Cart } from "../../models/cart";
 import { ProdutoDTO } from "../../models/produto.dto";
+import { Footer } from "ionic-angular";
 
 @Injectable()
 export class CartService {
@@ -28,5 +29,47 @@ export class CartService {
     }
     this.storage.setCart(cart);
     return cart;
+  }
+
+  removeProduto(produto: ProdutoDTO): Cart {
+    let cart = this.getCart();
+    let position = cart.items.findIndex(x => x.produto.id == produto.id);
+    if (position != -1) {
+      cart.items.splice(position, 1);
+    }
+    this.storage.setCart(cart);
+    return cart;
+  }
+
+  increaseQuantity(produto: ProdutoDTO): Cart {
+    let cart = this.getCart();
+    let position = cart.items.findIndex(x => x.produto.id == produto.id);
+    if (position != -1) {
+      cart.items[position].quantidade++;
+    }
+    this.storage.setCart(cart);
+    return cart;
+  }
+
+  decreaseQuantity(produto: ProdutoDTO): Cart {
+    let cart = this.getCart();
+    let position = cart.items.findIndex(x => x.produto.id == produto.id);
+    if (position != -1) {
+      cart.items[position].quantidade--;
+      if (cart.items[position].quantidade < 1) {
+        cart = this.removeProduto(produto);
+      }
+    }
+    this.storage.setCart(cart);
+    return cart;
+  }
+
+  total(): number {
+    let cart = this.getCart();
+    let sum = 0;
+    for (var i = 0; i < cart.items.length; i++) {
+      sum += sum + cart.items[i].produto.preco * cart.items[i].quantidade;
+    }
+    return sum;
   }
 }
